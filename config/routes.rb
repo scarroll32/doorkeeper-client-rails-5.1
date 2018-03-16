99917761c6eb69c21e5383dc972c7b2f0ab9bebf
Sean Carroll <sfcarroll@gmail.com>
2018-03-16 15:57:57 +0100

    obtain access token flow

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'pages/index'
   root to: 'pages#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/oauth/callback', to: 'sessions#create'
 end
